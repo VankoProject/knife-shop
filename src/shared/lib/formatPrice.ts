8caebceb916f989b0f0d00deee4d5524098e153c
Ivan Kliachenko <ivan.kliachenko.dev@gmail.com>
2026-04-20 23:11:10 +0300

    feat(format): add price formatter utility

---
+++ src/shared/lib/formatPrice.ts
@@ -0,0 +1,10 @@
+export function formatPrice(
+  value: number,
+  locale: string = 'en-US',
+  currency: string = 'USD'
+): string {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency
+  }).format(value)
+}
